fa2181f8442ea4b97c81ba4b4b1ba548a73c323f
Gelead worku <geleadw@gmail.com>
2025-07-21 12:55:22 +0300

    added toggle functionality on buttons

--- src/components/Assembly.jsx
+++ src/components/Assembly.jsx
@@ -2,7 +2,15 @@ import React, { useState } from "react";
 import { languages } from "./languages";
 
 const Assembly = () => {
-  const [words, setWords] = useState("react");
+  const [currentWord, setCurrentWord] = useState("react");
+  const [guessedLetters, setGuessedLetters] = useState([]);
+  function addGuessedLetters(letter) {
+    setGuessedLetters((prevLetters) =>
+      prevLetters.includes(letter) ? prevLetters : [...prevLetters, letter]
+    );
+  }
+
+  console.log(guessedLetters);
   const letters = [
     "A",
     "B",
@@ -58,30 +66,46 @@ const Assembly = () => {
           </span>
         ))}
       </section>
-      <section className="border p-2 flex gap-3 flex-wrap justify-center">
-        {words.split("").map((word) => (
+      <section className="border p-2 flex gap-2 flex-wrap justify-center">
+        {currentWord.split("").map((letter, index) => (
           <span
-            className="p-2 text-md border-b-2"
+            className="px-4 py-2 text-md border-b-2"
             style={{
               backgroundColor: "#111",
             }}
-            key={word}
+            key={index}
           >
-            {word.toUpperCase()}
+            {letter.toUpperCase()}
           </span>
         ))}
       </section>
       <section className="flex flex-wrap justify-center p-3 w-md gap-1 my-2">
-        {letters.map((letter) => (
-          <button
-            className="w-9 h-9 rounded-sm text-black bg-amber-600"
-            key={letter}
-          >
-            {letter}
-          </button>
-        ))}
+        {letters.map((letter) => {
+          const isGuessed = guessedLetters.includes(letter);
+          const isCorrect =
+            isGuessed && currentWord.includes(letter.toLowerCase());
+
+          return (
+            <button
+              onClick={() => addGuessedLetters(letter)}
+              className="w-9 h-9 rounded-sm text-black"
+              style={{
+                backgroundColor: isGuessed
+                  ? isCorrect
+                    ? "green"
+                    : "red"
+                  : "#f59e0b",
+              }}
+              key={letter}
+            >
+              {letter}
+            </button>
+          );
+        })}
       </section>
-      <button className="py-2 px-8 my-4 rounded-sm text-black bg-blue-600">New Game</button>
+      <button className="py-2 px-8 my-4 rounded-sm text-black bg-blue-600">
+        New Game
+      </button>
     </main>
   );
 };
